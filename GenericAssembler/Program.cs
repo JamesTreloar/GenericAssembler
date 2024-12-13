bc97f1047365ae9ec037defadaa3a2fd82313f6e
James Treloar <54793877+JamesTreloar@users.noreply.github.com>
2024-12-14 08:46:30 +1100

    Added command line argument parsing. (#8)

--- GenericAssembler/Program.cs
+++ GenericAssembler/Program.cs
@@ -1,41 +1,88 @@
-﻿namespace GenericAssembler; 
+﻿using CommandLine;
+
+namespace GenericAssembler;
 
 internal abstract class Program {
+	public class Options {
+		[Option(shortName: 'a', longName: "assembly", Required = false, HelpText = "Path to assembly file")]
+		public string? AssemblyPath { get; init; }
 
-	public static void Main(string[] args) {
-		ErrorValue ev;
-		if (args.Length != 2) {
-			ev = new(ErrorNumbers.InvalidOptions);
-			ev.DisplayError();
-			return;
+		[Option(shortName: 's', longName: "specification", Required = true, HelpText = "Path to specification file")]
+		public required string SpecificationPath { get; init; }
+
+		[Option(shortName: 'o', longName: "output", Required = false, HelpText = "Path to output file")]
+		public string? OutputPath { get; init; }
+
+		[Option(shortName: 'f', longName: "format", Required = false, HelpText = "Output format, b=1; h=2")]
+		public int Format { get; init; }
+	}
+
+	public static int Main(string[] args) {
+		Options? options = null;
+		Parser.Default.ParseArguments<Options>(args)
+			.WithParsed(
+				o => options = o
+			).WithNotParsed(err => {
+					foreach (Error error in err) {
+						Console.WriteLine(error.ToString());
+					}
+				}
+			);
+
+		if (options == null) {
+			return -1;
 		}
 
-		string jsonStringConfig = File.ReadAllText(args[0]); 
-		
+		ErrorValue ev;
+
+		string jsonStringConfig = File.ReadAllText(options.SpecificationPath);
+
 		SettingReader settingReader = new(jsonStringConfig);
-		
+
 		(Configuration? configuration, ev) = settingReader.Read();
 		if (!ev.IsOkay() || configuration == null) {
 			ev.DisplayError();
-			return;
+			return 1;
 		}
 
-		string[] input = File.ReadAllLines(args[1]);
+		if (options.AssemblyPath == null) {
+			Console.WriteLine("Specification file is correctly formed");
+			return 0;
+		}
+
+		string[] input = File.ReadAllLines(options.AssemblyPath);
 		ProcessFile processFile = new(configuration);
 		(List<string>? result, ev) = processFile.Run(input);
 		if (!ev.IsOkay() || result == null) {
 			ev.DisplayError();
-			return;
+			return 1;
 		}
 
-		Console.WriteLine("Binary representation: ");
-		foreach (string s in result) {
-			Console.WriteLine(s);
+		int format = options.Format;
+		if (options.OutputPath != null) {
+			if ((format & 1) == 1) {
+				File.WriteAllLines(options.OutputPath, result);
+			}
+
+			if ((format & 2) == 2) {
+				File.WriteAllLines(options.OutputPath, result.Select(x => "x" + Convert.ToInt32(x, 2).ToString("X")));
+			}
+
+			return 0;
 		}
-		
-		Console.WriteLine("Hex Representation: ");
-		foreach (string s in result) {
-			Console.WriteLine("x" + Convert.ToInt32(s, 2).ToString("X"));
+
+		if ((format & 1) == 1) {
+			foreach (string s in result) {
+				Console.WriteLine(s);
+			}
 		}
+
+		if ((format & 2) == 2) {
+			foreach (string s in result) {
+				Console.WriteLine("x" + Convert.ToInt32(s, 2).ToString("X"));
+			}
+		}
+
+		return 0;
 	}
 }
